e106c8c724c2f70b3b9dd9d12e643611b0648367
1490340403 <1490340403@qq.com>
2020-11-10 07:34:38 +0800

    'header处理'

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { bulidURL } from './helpers/url'
 import {AxiosConfig} from './types'
 import xhr from './xhr' 
 import {transformData} from './helpers/data'
+import {processHeaders} from './helpers/headers'
 function axios (config:AxiosConfig):void{
     processConfig(config)
     xhr(config)
@@ -9,6 +10,7 @@ function axios (config:AxiosConfig):void{
 function processConfig (config: AxiosConfig): void {
     config.url = transformUrl(config)
     config.data=transformRequsetData(config)
+    config.headers=transformHeaders(config)
   }
   
 function transformUrl (config: AxiosConfig): string {
@@ -17,5 +19,9 @@ function transformUrl (config: AxiosConfig): string {
 }
 function transformRequsetData(config: AxiosConfig){
     return transformData(config.data)
+}
+function transformHeaders(config: AxiosConfig){
+    const {headers={},data}=config
+    return processHeaders(headers,data)
 }
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,7 +1,16 @@
 import {AxiosConfig} from './types'
 export default function xhr (config:AxiosConfig):void{
-    const {method='get',data=null,url}=config
+    const {method='get',data=null,url,headers}=config
     const request= new XMLHttpRequest()
     request.open(method.toLocaleUpperCase(),url,true)
+    Object.keys(headers).forEach(item=>{
+        console.log(headers[item],'headers[item]')
+        if(data==null&&item.toLowerCase() === 'content-type'){
+            delete headers[item]
+        }else {
+            
+            request.setRequestHeader(item, headers[item])
+          }
+    })
     request.send(data)
 }
